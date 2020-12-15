dd0a66ce64a09de8befcd98ac6e919bc57e02b02
aishaullybux <47082346+aishaullybux@users.noreply.github.com>
2020-12-15 16:22:19 +0000

    Add files via upload

---
+++ sine_wave.m
@@ -0,0 +1,16 @@
+number_of_periods = 10
+
+%actual code to use in final to make array of 7 kHz sine values
+t = 0:1/44100:number_of_periods/7000;
+y = sin(2*pi*7000*t);
+% yes it's just 2 lines
+
+%adding 2 arrays 1 after each other to test if it matches actual sine
+samples = [y y]
+time = [t (10/7000)+t]
+plot(time,samples)
+hold on
+
+t = 0:1/44100:2*number_of_periods/7000
+y = sin(2*pi*7000*t)
+plot(t,y)
