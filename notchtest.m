4c0dd657001c4eabfdbe495adc3d81e4debe29fa
AlWestwood <76054678+AlWestwood@users.noreply.github.com>
2020-12-15 15:00:05 +0000

    working fft

--- notchtest.m
+++ notchtest.m
@@ -1,16 +1,22 @@
-% [b,a] = cheby2(5,80,[7950/22050 8050/22050],'stop');
+[b,a] = cheby2(5,80,[7950/22050 8050/22050],'stop');
 
-[h,t] = impz(notch,1024,44100);
+L = 1024;
+fs = 44100;
+f = fs * (0:(L/2))/L;
+
+[h,t] = impz(b,a,1024,44100);
 figure;
 plot(t,h,'k-')
 
-y = sin(2.*pi.*5000.*t);
-yfft = fft(y)
-Py = abs(yfft)
-f = [1:11000/1024:11000];
+y = sin(2*pi*5500*t)+sin(2*pi*6000*t)+sin(2*pi*6500*t)+sin(2*pi*7000*t)+sin(2*pi*7500*t)+sin(2*pi*8000*t)+sin(2*pi*8500*t)+sin(2*pi*9000*t)+sin(2*pi*9500*t)+sin(2*pi*10000*t)+sin(2*pi*10500*t)+sin(2*pi*11000*t);
+yfft = fft(y);
+P2 = abs(yfft/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+
 figure(2);
 plot(t,y,'k-');
 xlim([0 0.001]);
 
 figure(3);
-plot(f,Py,'k-');
+plot(f,P1,'k-');
